7b38f661cd55f4a8ad863831d24fe5a58ff3fcdf
JessicaNovo <50515278+JessicaNovo@users.noreply.github.com>
2019-12-01 20:13:43 +0000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/JessicaNovo/react-weather-project"
+          target="/"
+        >
+          Open-source code
+        </a>{" "}
+        by Jéssica Novo
+      </footer>
+    </div>
+  );
 }
