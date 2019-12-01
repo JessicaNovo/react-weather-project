4d068a3d871ffc5b4221da94cdea63012d4546b2
JessicaNovo <50515278+JessicaNovo@users.noreply.github.com>
2019-12-01 20:39:33 +0000

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <h1>Weather App</h1>
       <footer>
         <a
