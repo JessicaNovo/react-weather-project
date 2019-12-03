b7fb3d8d75cbe2477411d09524cd6532fc01dcda
JessicaNovo <50515278+JessicaNovo@users.noreply.github.com>
2019-12-03 02:41:21 +0000

    Implemented basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -5,31 +5,47 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input type="search" placeholder="Enter a city..." />
-        <input type="submit" value="Search" />
-        <button>Current</button>
+        <div className="form-row">
+          <div className="form-group col-md-8">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="form-group col-md-2">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-secondary w-100"
+            />
+          </div>
+          <div className="form-group col-md-2">
+            <button className="btn btn-secondary w-100">Current</button>
+          </div>
+        </div>
       </form>
       <div className="row align-items-center">
-        <div className="col-4">
+        <div className="col-sm-4">
           <h1>Póvoa de Varzim</h1>
-          <h2>Sunday</h2>
-          <h2>December 1</h2>
-          <p>Last updated: 22h00</p>
+          <h2>Sunday, December 1</h2>
+          <p className="updated">Last updated: 22h00</p>
         </div>
-        <div className="col-4">
+        <div className="col-sm-4 current-weather">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
             alt=""
           />
-          <p>Clear</p>
           <div>
-            12
-            <small>
+            <p className="weather-description">Clear</p>
+            <span className="current-temperature">12</span>
+            <small className="temperature-units">
               ºC | <a href="/">ºF</a>
             </small>
           </div>
         </div>
-        <div className="col-4">
+        <div className="col-sm-4">
           <ul>
             <li>Sunrise: 07h36</li>
             <li>Sunset: 17h14</li>
